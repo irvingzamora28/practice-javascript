1245559c471b3b68fb51eae134cd8003d6583f00
Irving Zamora <irvingzamora28@gmail.com>
2022-02-04 20:47:11 -0700

    Add Flexbox section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Flexbox from './sections/Flexbox';
 
 function App() {
   return (
     <div className="container">
-        Hello World
+        <Flexbox />
     </div>
   );
 }
---
+++ src/sections/Flexbox.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../resources/styles/Flexbox.css';
+
+const Flexbox = () => {
+	return (
+		<div className="main-container">
+			<div className="flex-container">
+				<div className="flex-item">Item 1</div>
+				<div className="flex-item">Item 2</div>
+				<div className="flex-item">Item 3</div>
+				{/* <div className="flex-item">Item 4</div>
+				<div className="flex-item">Item 5</div>
+				<div className="flex-item">Item 6</div>
+				<div className="flex-item">Item 7</div>
+				<div className="flex-item">Item 8</div> */}
+			</div>
+		</div>
+	);
+};
+
+export default Flexbox;
